2b602786e925dba2b7b571bb302744a0d29468cd
pgaborca <pgaborca@yahoo.com>
2019-05-31 06:59:56 +0200

    Lesson 92

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,3 +1,15 @@
 export function isEmpty(val) {
   return val.trim().length === 0;
 }
+
+export function isValidEmail(val) {
+  return new RegExp(
+    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+  ).test(val);
+}
+
+export function isValidUrl(val) {
+  return new RegExp(
+    "((?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})"
+  ).test(val);
+}
